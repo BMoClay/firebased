c6d3b8ac8ed4df92b53b769131f2be9c5e699ae7
BMoClay <benmorgancleveland@gmail.com>
2023-11-17 11:20:29 -0500

    emojis fetch from firestore

--- index.js
+++ index.js
@@ -6,7 +6,12 @@ import { getAuth,
     onAuthStateChanged,
     GoogleAuthProvider,
     signInWithPopup } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-auth.js"
-import { getFirestore } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"
+import { getFirestore, 
+    collection,
+    addDoc,
+    serverTimestamp,
+    getDocs } from "https://www.gstatic.com/firebasejs/10.6.0/firebase-firestore.js"
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDyRJa-0-F05apyE-z3iiHNxM_1J6yeRXY",
@@ -30,17 +35,25 @@ const viewLoggedOut = document.getElementById("logged-out-view")
 const viewLoggedIn = document.getElementById("logged-in-view")
 
 const signInWithGoogleButtonEl = document.getElementById("sign-in-with-google-btn")
+
 const emailInputEl = document.getElementById("email-input")
 const passwordInputEl = document.getElementById("password-input")
+
 const signInButtonEl = document.getElementById("sign-in-btn")
 const createAccountButtonEl = document.getElementById("create-account-btn")
+
 const signOutButtonEl = document.getElementById("sign-out-btn")
+
 const userProfilePictureEl = document.getElementById("user-profile-picture")
 const userGreetingEl = document.getElementById("user-greeting")
 
-// const displayNameInputEl = document.getElementById("display-name-input")
-// const photoURLInputEl = document.getElementById("photo-url-input")
-// const updateProfileButtonEl = document.getElementById("update-profile-btn")
+const moodEmojiEls = document.getElementsByClassName("mood-emoji-btn")
+const textareaEl = document.getElementById("post-input")
+const postButtonEl = document.getElementById("post-btn")
+
+const fetchPostsButtonEl = document.getElementById("fetch-posts-btn")
+
+const postsEl = document.getElementById("posts")
 
 /* == UI - Event Listeners == */
 
@@ -48,8 +61,20 @@ signInWithGoogleButtonEl.addEventListener("click", authSignInWithGoogle)
 
 signInButtonEl.addEventListener("click", authSignInWithEmail)
 createAccountButtonEl.addEventListener("click", authCreateAccountWithEmail)
+
 signOutButtonEl.addEventListener("click", authSignOut)
-// updateProfileButtonEl.addEventListener("click", authUpdateProfile)
+
+for (let moodEmojiEl of moodEmojiEls) {
+    moodEmojiEl.addEventListener("click", selectMood)
+}
+
+postButtonEl.addEventListener("click", postButtonPressed)
+
+fetchPostsButtonEl.addEventListener("click", fetchOnceAndRenderPostsFromDB)
+
+/* === State === */
+
+let moodState = 0
 
 /* === Main Code === */
 
@@ -124,7 +149,58 @@ function authSignOut() {
 //     })
 // }
 
+/* = Functions - Firebase - Cloud Firestore = */
+
+async function addPostToDB(postBody, user) {
+    try {
+        const docRef = await addDoc(collection(db, "posts"), {
+            body: postBody,
+            uid: user.uid,
+            createdAt: serverTimestamp(),
+            mood: moodState
+        });
+        console.log("Document written with ID: ", docRef.id);
+    } catch (error) {
+        console.error(e.message);
+    }
+
+}
+
+async function fetchOnceAndRenderPostsFromDB() {
+    const querySnapshot = await getDocs(collection(db, "posts"))
+    
+    querySnapshot.forEach((doc) => {
+        renderPost(postsEl, doc.data())
+    })
+}
+
 /* == Functions - UI Functions == */
+
+function renderPost(postsEl, postData) {
+    postsEl.innerHTML += `
+        <div class="post">
+            <div class="header">
+                <h3>${displayDate(postData.createdAt)}</h3>
+                <img src="assets/emojis/${postData.mood}.png">
+            </div>
+            <p>
+                ${postData.body}
+            </p>
+        </div>
+    `
+}
+
+
+function postButtonPressed() {
+    const postBody = textareaEl.value
+    const user = auth.currentUser
+
+    if (postBody && moodState) {
+        addPostToDB(postBody, user)
+        clearInputField(textareaEl)
+        resetAllMoodElements(moodEmojiEls)
+    }
+}
 
 function showLoggedOutView() {
     hideView(viewLoggedIn)
@@ -173,4 +249,57 @@ function showUserGreeting(element, user) {
     } else {
         element.textContent = `Hey friend, how are you?`
     }
+}
+
+function displayDate(firebaseDate) {
+    const date = firebaseDate.toDate()
+    
+    const day = date.getDate()
+    const year = date.getFullYear()
+    
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+    const month = monthNames[date.getMonth()]
+
+    // let hours = date.getHours()
+    // let minutes = date.getMinutes()
+    // hours = hours < 10 ? "0" + hours : hours
+    // minutes = minutes < 10 ? "0" + minutes : minutes
+    // return `${day} ${month} ${year} - ${hours}:${minutes}`
+    return `${day} ${month} ${year}`
+}
+/* = Functions - UI Functions - Mood = */
+
+function selectMood(event) {
+    const selectedMoodEmojiElementId = event.currentTarget.id
+    
+    changeMoodsStyleAfterSelection(selectedMoodEmojiElementId, moodEmojiEls)
+    
+    const chosenMoodValue = returnMoodValueFromElementId(selectedMoodEmojiElementId)
+    
+    moodState = chosenMoodValue
+}
+
+function changeMoodsStyleAfterSelection(selectedMoodElementId, allMoodElements) {
+    for (let moodEmojiEl of moodEmojiEls) {
+        if (selectedMoodElementId === moodEmojiEl.id) {
+            moodEmojiEl.classList.remove("unselected-emoji")          
+            moodEmojiEl.classList.add("selected-emoji")
+        } else {
+            moodEmojiEl.classList.remove("selected-emoji")
+            moodEmojiEl.classList.add("unselected-emoji")
+        }
+    }
+}
+
+function resetAllMoodElements(allMoodElements) {
+    for (let moodEmojiEl of allMoodElements) {
+        moodEmojiEl.classList.remove("selected-emoji")
+        moodEmojiEl.classList.remove("unselected-emoji")
+    }
+    
+    moodState = 0
+}
+
+function returnMoodValueFromElementId(elementId) {
+    return Number(elementId.slice(5))
 }
